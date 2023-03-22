b75adec94c610ddd2545794211cded6579bef738
Sofenos <soufianainos@gmail.com>
2023-03-21 19:29:44 -0700

    spinozadone

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    times_table();
-    return (0);
-}
+
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
+	{
+	    times_table();
+	    return (0);
+	}
