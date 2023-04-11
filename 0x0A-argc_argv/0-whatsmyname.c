41a3179ded3bddc12fbfd2233cabd0ec4f79c10d
Sofenos <soufianainos@gmail.com>
2023-04-10 17:25:46 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -> Program
+ *
+ * @argc: Size array argv
+ * @argv: Array of size argc
+ *
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
