aed5f3d1bb49c6d25e3d60071b3f5b29f3b09fe0
Sofenos <soufianainos@gmail.com>
2023-04-14 19:17:39 -0700

    slymoalpha

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (nptr);
 		for (i = 0; i < old_size; i++)
 			nptr[i] = *((char *)ptr + 1);
-		free(ptr);
+				free(ptr);
 	}
 
 	return (nptr);
