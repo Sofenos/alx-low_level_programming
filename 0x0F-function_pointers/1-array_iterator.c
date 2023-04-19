11ba59db36340876182bafc2e16b53c369f8f70a
Sofenos <soufianainos@gmail.com>
2023-04-19 15:25:10 -0700

    int index

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - print each array element on a new line
+ * @array: array
+ * @size: how many element to print
+ * @action: pointer to print in regular
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
